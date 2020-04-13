2ee587d59c198a415565651efcfcb34694bccc0a
Sandra <sandra.boez@gmail.com>
2020-04-13 15:22:22 +0200

    :tada: add initial game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Memory-Game
+
+Memory card game made to improve my skills, done with [this tutorial](https://github.com/code-sketch/memory-game)
---
+++ script.js
@@ -0,0 +1,53 @@
+const cards = document.querySelectorAll('.memory-card');
+let isFlipped = false;
+let locked = false;
+let firstCard, secondCard;
+
+function flipCard() {
+	if (locked) return;
+	if (this === firstCard) return;
+
+	this.classList.add('flip');
+	if (!isFlipped) {
+		isFlipped = true;
+		firstCard = this;
+	} else {
+		isFlipped = false;
+		secondCard = this;
+		checkMatch();
+	}
+}
+
+function checkMatch() {
+	firstCard.dataset.framework === secondCard.dataset.framework ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
+	reset();
+}
+
+function unflipCards() {
+	locked = true;
+	setTimeout(() => {
+		firstCard.classList.remove('flip');
+		secondCard.classList.remove('flip');
+		reset();
+	}, 700);
+}
+
+function reset() {
+	[isFlipped, locked] = [false, false];
+	[firstCard, secondCard] = [null, null];
+}
+
+// IIFE: Immediately Invoked Function Expression
+(function shuffle() {
+	cards.forEach(card => {
+		let randomPos = Math.floor(Math.random() * 12);
+		card.style.order = randomPos;
+	});
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
